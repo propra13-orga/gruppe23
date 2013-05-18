5382d1a599ea3c1ae60111cf1781c4af47e0702f
Maike Fox <maike.fox@uni-duesseldorf.de>
2013-05-18 18:31:54 +0200

    Brigis Teil

---
+++ main_package/Gameframe.java
@@ -0,0 +1,49 @@
+package main_package;
+
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+
+public class Gameframe extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BufferStrategy strat;
+	
+	public Gameframe(){
+		
+		super("Dungeon Crawler");
+	}
+	
+	public void makestrat(){
+		createBufferStrategy(2);
+		strat = getBufferStrategy();
+		
+	}
+	
+	public void repaint(){
+		
+		Graphics g = strat.getDrawGraphics();
+		draw(g);
+		g.dispose();
+		strat.show();
+	}
+	
+	public void draw(Graphics g){
+		
+
+	}
+	
+	public void update(){
+		
+		
+	}
+	
+
+}
+
